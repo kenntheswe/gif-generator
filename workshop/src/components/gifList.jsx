fcb8ec7781772fa007a390aa901877276656dfa1
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-06-18 15:18:57 +0900

    Added giflist using props

--- workshop/src/components/gifList.jsx
+++ workshop/src/components/gifList.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 import Gif from './gif';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class GifList extends Component {
   render() {
     const ids = ["gG6OcTSRWaSis", "13HgwGsXF0aiGY", "13UZisxBxkjPwI", "zOvBKUUEERdNm"];
     return (
       <div className="gif-list">
         { ids.map((id) => {
-          <Gif id={id} key={id}/>
+          return <Gif id={id} key={id} />
         })}
       </div>
     )
