5859feafc3142179d206d0d26d98886f04445c4e
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-06-19 16:57:10 +0900

    Fixed styles on index jsx

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -2,7 +2,7 @@ import '../assets/stylesheets/application.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app'
+import App from './components/app';
 
 const root = document.querySelector('#root');
-ReactDOM.render(<App />, root)
+ReactDOM.render(<App />, root);
