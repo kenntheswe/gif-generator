e4971514ad44cfb0182fac3dbffb297176c4dabe
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-06-19 18:46:54 +0900

    Added more gifs & customized css

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -5,11 +5,12 @@ export default class SearchBar extends Component {
     const { changeGifIds } = this.props;
     changeGifIds(event.currentTarget.value);
   }
+
   render() {
     return (
       <div>
-        <input className="form-search form-control" type="text" placeholder="Search GIF" onChange={this.handleChange}/>
+        <input className="form-search form-control" type="text" placeholder="Search GIF" onChange={this.handleChange} />
       </div>
-    )
+    );
   }
 }
